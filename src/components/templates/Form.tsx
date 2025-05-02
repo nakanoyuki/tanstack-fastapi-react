81d9ed1271e87fc99a5cc6e4c6816e56c0eac17b
nakanoyuki <yukich41@gmail.com>
2025-05-02 21:34:39 +0900

    性別のラジオボタン追加

--- src/components/templates/Form.tsx
+++ src/components/templates/Form.tsx
@@ -4,6 +4,7 @@ const Form = () => {
   const [form, setForm] = useState({
     name: "",
     age: "",
+    gender: "",
   });
   const handleChange = (
     e: ChangeEvent<HTMLSelectElement | HTMLInputElement>
@@ -17,13 +18,14 @@ const Form = () => {
         <label htmlFor="name">
           名前
           <input
+            name="name"
             id="name"
             type="text"
-            name="name"
             value={form.name}
             onChange={handleChange}
           />
         </label>
+        <br />
         <label htmlFor="age">
           年齢
           <select name="age" id="age" value={form.age} onChange={handleChange}>
@@ -32,7 +34,40 @@ const Form = () => {
             <option value={30}>30代</option>
           </select>
         </label>
+        <br />
+        <label htmlFor="age">
+          性別
+          <input
+            name="gender"
+            id="male"
+            type="radio"
+            value="male"
+            onChange={handleChange}
+          />
+          男性
+          <input
+            name="gender"
+            id="female"
+            type="radio"
+            value="female"
+            onChange={handleChange}
+          />
+          女性
+          <input
+            name="gender"
+            id="other"
+            type="radio"
+            value="other"
+            onChange={handleChange}
+          />
+          その他
+        </label>
       </form>
+      <p>
+        {form.name}
+        {form.age}
+        {form.gender}
+      </p>
     </>
   );
 };
