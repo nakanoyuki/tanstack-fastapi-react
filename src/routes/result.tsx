1214146f0685801cdb937891e1c9660eff55ba5f
nakanoyuki <yukich41@gmail.com>
2025-05-08 00:07:36 +0900

    useSearch を型安全に使うためにvalidateSearchを定義

--- src/routes/result.tsx
+++ src/routes/result.tsx
@@ -1,7 +1,22 @@
 import { createFileRoute } from "@tanstack/react-router";
 import Result from "../components/templates/Result";
 
+type SearchParams = {
+  name: string;
+  age: string;
+  gender: string;
+  comment: string;
+};
+
 export const Route = createFileRoute("/result")({
+  validateSearch: (search: Record<string, unknown>): SearchParams => {
+    return {
+      name: String(search.name ?? ""),
+      age: String(search.age ?? ""),
+      gender: String(search.gender ?? ""),
+      comment: String(search.comment ?? ""),
+    };
+  },
   component: ResultComponent,
 });
 
